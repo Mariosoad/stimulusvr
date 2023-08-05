b73febf5d7653fc0de6cddd71da9bba7fa64cbb3
Mario Hinostroza <mario.bba@hotmail.com>
2023-08-05 01:47:51 -0300

    update stimulus

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -2,6 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter } from 'react-icons/bs';
+import { BsYoutube } from 'react-icons/bs';
+import { BsVimeo } from 'react-icons/bs';
+
 import "./footer.css"
 
 export default function Footer() {
@@ -9,7 +14,13 @@ export default function Footer() {
         <div className='container-footer'>
             <div className='child-container-footer'>
                 <div>
-                    <Image width={185} height={60} src="https://stimulusvr.com/images/logoloco.svg" />
+                    <Image width={200} height={70} src="https://stimulusvr.com/images/logoloco.svg" />
+                    <ul class="social-networks">
+                        <li><a href="https://www.facebook.com/stimulusvr/"><FaFacebookF /></a></li>
+                        <li><a href="https://twitter.com/stimulusvr"><BsTwitter /></a></li>
+                        <li><a href="https://www.youtube.com/watch?v=rgxx-g8stTU"><BsYoutube /></a></li>
+                        <li><a href="https://vimeo.com/395997583"><BsVimeo /></a></li>
+                    </ul>
                 </div>
                 <div className='container-links'>
                     <div>
